b34a8c2a98aad0909ed5d84b777d997769728b68
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2019-12-11 11:15:43 +0400

    change data url

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,4 +1,6 @@
 // url
-export default "https://react-vintage-tech-storet-api.herokuapp.com";
+export default "https://johnsmilgatutorials.com/projects/react-tech-store-v2";
+
+// "https://react-vintage-tech-storet-api.herokuapp.com";
 
 // https://johnsmilgatutorials.com/projects/react-tech-store-v2
